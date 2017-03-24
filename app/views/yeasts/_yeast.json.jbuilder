98d0d8dadff2170c86786d413b729a5cc07a6b9a
StaphSynth <StaphSynth@users.noreply.github.com>
2017-03-24 11:04:51 +1100

    change yeast 'type' column to 'fermentation_type'

--- app/views/yeasts/_yeast.json.jbuilder
+++ app/views/yeasts/_yeast.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! yeast, :id, :name, :type, :temp_range, :description, :created_at, :updated_at
+json.extract! yeast, :id, :name, :fermentation_type, :temp_range, :description, :created_at, :updated_at
 json.url yeast_url(yeast, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323235128) do
+ActiveRecord::Schema.define(version: 20170323235911) do
 
   create_table "hops", force: :cascade do |t|
     t.string   "name"
@@ -35,11 +35,11 @@ ActiveRecord::Schema.define(version: 20170323235128) do
 
   create_table "yeasts", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "fermentation_type"
     t.string   "temp_range"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
 end
