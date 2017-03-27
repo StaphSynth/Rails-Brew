a9c2ceb47f57077637704e8556e3ca972a716e73
Staphsynth <1337dallen@gmail.com>
2017-03-27 13:51:36 +1100

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  before_save do
+    self.email = self.email.downcase
+  end
+
+  validates :name,  presence: true,
+                    length: {maximum: 50}
+
+  validates :email, presence: true,
+                    length: {maximum: 255},
+                    format: {:with => EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327003606) do
+ActiveRecord::Schema.define(version: 20170327021817) do
 
   create_table "hops", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170327003606) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "yeasts", force: :cascade do |t|
