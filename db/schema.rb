0ad40c5e79104aedbbf98796564e41cdc764ab5b
Staphsynth <1337dallen@gmail.com>
2017-03-27 12:15:11 +1100

    add user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324001534) do
+ActiveRecord::Schema.define(version: 20170327003606) do
 
   create_table "hops", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170324001534) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "yeasts", force: :cascade do |t|
