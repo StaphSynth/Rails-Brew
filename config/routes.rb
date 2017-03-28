190a037182c2ce07ea60a2e86bf498a19c684123
Staphsynth <1337dallen@gmail.com>
2017-03-28 13:13:33 +1100

    fix signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'signup', to: 'static_pages#signup'
+  get 'signup', to: 'users#new'
   get 'home', to: 'static_pages#home'
 
   resources :users
