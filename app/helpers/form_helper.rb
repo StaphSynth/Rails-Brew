757fa7778074c5af5ae60187594f616470da81bb
Staphsynth <1337dallen@gmail.com>
2017-04-01 17:34:14 +1100

    add helper for recipe creation

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,10 +1,6 @@
 module FormHelper
   def setup_recipe(recipe)
     recipe.recipe_ingredients ||= RecipeIngredient.new
-
-    3.times do
-      recipe.recipe_ingredients.build
-    end
     return recipe
   end
 end
