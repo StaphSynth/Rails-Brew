56df5c96d8796d7e0907a8c7dbe896ad83c30984
StaphSynth <StaphSynth@users.noreply.github.com>
2017-04-02 22:58:38 +1000

    clear value of input field on clone

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,6 +1,7 @@
 console.log('recipes.js');
 
 
+
 $(document).on('turbolinks:load', function() {
   $('.add-ingredient-btn').click(function() {
     var replaceExp = new RegExp(/[0-9]+/);
@@ -8,7 +9,7 @@ $(document).on('turbolinks:load', function() {
     var $newIngredient = $($lastIngredient).clone();
     var totalIngredients = $('.ingredient-selection').length.toString();
 
-    $($newIngredient).find('select, input').each(function() {
+    $($newIngredient).find('select, input').each(function(elem) {
       var oldId = $(this).attr('id');
       var newId = oldId.replace(replaceExp, totalIngredients);
       $(this).attr('id', newId);
@@ -16,6 +17,9 @@ $(document).on('turbolinks:load', function() {
       var oldName = $(this).attr('name');
       var newName = oldName.replace(replaceExp, totalIngredients);
       $(this).attr('name', newName);
+
+      if($(this).is('input'))
+        $(this).val('');
     });
 
     $($newIngredient).insertAfter($lastIngredient);
