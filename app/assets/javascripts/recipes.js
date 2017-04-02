abdbaf5ab3d8fe1db1b8e6adcf368f97aa85f22d
StaphSynth <StaphSynth@users.noreply.github.com>
2017-04-02 21:30:57 +1000

    added ability for user to add more ingredients selection form elements to recipe forms

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,23 @@
+console.log('recipes.js');
+
+
+$(document).on('turbolinks:load', function() {
+  $('.add-ingredient-btn').click(function() {
+    var replaceExp = new RegExp(/[0-9]+/);
+    var $lastIngredient = $('.ingredient-selection').last();
+    var $newIngredient = $($lastIngredient).clone();
+    var totalIngredients = $('.ingredient-selection').length.toString();
+
+    $($newIngredient).find('select, input').each(function() {
+      var oldId = $(this).attr('id');
+      var newId = oldId.replace(replaceExp, totalIngredients);
+      $(this).attr('id', newId);
+
+      var oldName = $(this).attr('name');
+      var newName = oldName.replace(replaceExp, totalIngredients);
+      $(this).attr('name', newName);
+    });
+
+    $($newIngredient).insertAfter($lastIngredient);
+  });
+});
