303cbe617f328bb3bf80aff5f56f6f3f550b7705
Staphsynth <1337dallen@gmail.com>
2017-04-03 16:04:29 +1000

    create stock_items controller

---
+++ app/controllers/stock_items_controller.rb
@@ -0,0 +1,2 @@
+class StockItemsController < ApplicationController
+end
--- test/controllers/stock_items_controller_test.rb
+++ test/controllers/stock_items_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StockTest < ActiveSupport::TestCase
+class StockItemsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
