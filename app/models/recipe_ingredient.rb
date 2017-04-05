8ed587de6272314056a42f155000ae6a83e7c0cb
Staphsynth <1337dallen@gmail.com>
2017-04-05 11:31:51 +1000

    add verification to model

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,6 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
 
   validates :quantity, :numericality => {:greater_than => 0}
+  validates_presence_of :ingredient_id
+  validates_presence_of :recipe_id
 end
