9883b808f54c705d502835199ee0257b6cc580e7
Staphsynth <1337dallen@gmail.com>
2017-04-07 13:03:12 +1000

    remove redundant models

--- app/models/recipe_ingredient.rb
+++
@@ -1,8 +0,0 @@
-class RecipeIngredient < ApplicationRecord
-  has_one :ingredient
-  belongs_to :recipe
-
-  validates :quantity, :numericality => {:greater_than => 0}
-  validates_presence_of :ingredient_id
-  validates_presence_of :recipe_id
-end
