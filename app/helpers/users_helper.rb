506752b492aea8b7153d8fab895f78ddeca93a22
StaphSynth <StaphSynth@users.noreply.github.com>
2017-04-11 10:51:39 +1000

    improve user edit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  #returns inner HTML text for the user account submit button
+  def submit_wording(mode)
+    if mode == :edit
+      return 'Update'
+    elsif mode == :new
+      return 'Create'
+    end
+  end
 end
