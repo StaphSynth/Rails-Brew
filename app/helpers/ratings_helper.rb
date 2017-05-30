20a5e6c5cb6be08a24e2a6d690df672f12e36722
Staphsynth <1337dallen@gmail.com>
2017-05-30 14:06:02 +1000

    remove obsolete helper function

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -15,15 +15,4 @@ module RatingsHelper
     return false if !logged_in?
     Rating.find_by(:recipe_id => recipe.id, :user_id => user.id) ? true : false
   end
-
-  #returns the options hash for the form_for helper
-  def get_options_hash(type, rating = nil)
-    case type
-    when :create
-      return {:url => {:controller => 'ratings', :action => 'create'}, :html => {:method => :post}}
-    when :edit
-      return {:url => {:controller => 'ratings', :action => 'update', :id => rating.id}, :html => {:method => :put}}
-    end
-  end
-
 end
