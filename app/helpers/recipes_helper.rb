9f9c3cdb21eae00bebdee15f937c46fee312334c
Staphsynth <1337dallen@gmail.com>
2017-06-09 16:29:24 +1000

    add drop-down select for recipe style

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -4,4 +4,19 @@ module RecipesHelper
   def get_styles
     JSON.parse File.read('app/data/style_data.json')
   end
+
+  def generate_style_options
+    options = Array.new
+
+    get_styles.each do |key, value|
+      option = Array.new
+      option.push(value['name'])
+      option.push key
+
+      options.push option
+    end
+
+    return options
+  end
+
 end
