5b08ad4b8c5348dc433a1d82003842bb1b229f09
Staphsynth <1337dallen@gmail.com>
2017-06-09 17:09:04 +1000

    use OpenStruct instead of hashes for recipe style data

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -19,9 +19,9 @@ module RecipesHelper
     return options
   end
 
-  #returns a hash containing style data when passed a BJCP subcategory id as a string
+  #returns an containing style data-accessing methods when passed a BJCP subcategory id as a string
   def get_style(id)
-    get_styles[id]
+    return OpenStruct.new(get_styles[id])
   end
 
 end
