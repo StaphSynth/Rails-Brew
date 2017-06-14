79b5549a88ef25fb47e2613e2cb5b98ff7f67ac9
StaphSynth <StaphSynth@users.noreply.github.com>
2017-06-14 22:09:19 +1000

    add validation to user_preferences model

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -1,3 +1,8 @@
 class UserPreference < ApplicationRecord
   belongs_to :user
+
+  validates :volume, length: {is: 1}, format: {with: /[LG]/} # L = litres, G = gallons
+  validates :temp, length: {is: 1}, format: {with: /[CF]/}   # C = celcius, F = fahrenheit
+  validates :weight, length: {is: 1}, format: {with: /[MI]/} # M = metric (g/kg), I = imperial (lbs/oz)
+  validates :default_efficiency, :numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
