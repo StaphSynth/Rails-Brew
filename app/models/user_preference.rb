b5db4a194776306bf2fd7a0a9ae1527d2c450e90
StaphSynth <StaphSynth@users.noreply.github.com>
2017-06-14 22:23:43 +1000

    fix bug in user_pref model

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -4,5 +4,5 @@ class UserPreference < ApplicationRecord
   validates :volume, length: {is: 1}, format: {with: /[LG]/} # L = litres, G = gallons
   validates :temp, length: {is: 1}, format: {with: /[CF]/}   # C = celcius, F = fahrenheit
   validates :weight, length: {is: 1}, format: {with: /[MI]/} # M = metric (g/kg), I = imperial (lbs/oz)
-  validates :default_efficiency, :numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :default_efficiency, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
