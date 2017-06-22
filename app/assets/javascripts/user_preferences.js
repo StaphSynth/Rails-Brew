17672ea3e1e590e00961af32bfad1b62c45f2f3b
Staphsynth <1337dallen@gmail.com>
2017-06-22 18:40:56 +1000

    auto change user pref volume units on selection change

--- app/assets/javascripts/user_preferences.js
+++ app/assets/javascripts/user_preferences.js
@@ -1,11 +1,31 @@
 $(document).on('turbolinks:load', function() {
 
   //setup display of batch volume
-  $('#batch-display').val(unitConverter['L'][gon.userPref.volume]($('#batch-model').val()));
+  setVolumeDisplay();
 
   //link the batch volume display with the model
   $('#batch-display').change(function() {
     $('#batch-model').val(unitConverter[gon.userPref.volume]['L']($(this).val()));
   });
 
+  //if the user changes the volume radio,
+  //automatically change the units on the default batch volume
+  $('input[name*="user_preference[volume]"]').change(function() {
+    var userSelection = $(this).val();
+    var unitSymb = {
+      B: 'Imp. Gal.',
+      G: 'Gal.',
+      L: 'L'
+    };
+
+    gon.userPref.volume = userSelection;
+    $('.volume-unit').html(unitSymb[userSelection]);
+    setVolumeDisplay();
+  });
+
 });
+
+function setVolumeDisplay() {
+  var value = unitConverter['L'][gon.userPref.volume](parseFloat($('#batch-model').val())).toFixed(1);
+  $('#batch-display').val(value);
+}
