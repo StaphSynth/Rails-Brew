027d080ffe8ad6f0c47df59b536f37d2adf3f0a4
Staphsynth <1337dallen@gmail.com>
2017-08-19 17:17:04 +1000

    Put RecipeMetaPanel component in its own file

--- client/app/bundles/RecipeForm/components/RecipeForm.jsx
+++ client/app/bundles/RecipeForm/components/RecipeForm.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import BrewCalc from '../brewCalcs';
-import glass from '../img/glass_small.png';
+import RecipeMetaPanel from './RecipeMetaPanel';
 
 export default class RecipeForm extends React.Component {
   constructor(props) {
@@ -33,74 +33,6 @@ export default class RecipeForm extends React.Component {
   render() {
     return (
       <RecipeMetaPanel recipe={ this.state } parentCallback={ (data) => this.childCallback(data) } />
-    );
-  }
-}
-
-class RecipeMetaPanel extends React.Component {
-  constructor(props) {
-    super(props);
-    var fg = this.props.recipe.FG || '0';
-
-    this.state = {
-      fgPrevious: fg,  //the last known good fg value
-      fgInput: fg,     //the fg value displayed in the field
-      fgValid: true
-    }
-  }
-
-  //updates the state with a new valid FG value is entered
-  //passes the new FG value up to the parent RecipeForm Component
-  updateFg(e) {
-    var fg = e.target.value;
-    var valid = BrewCalc.validateFg(fg) && (this.props.recipe.OG > fg);
-
-    this.setState({
-      fgInput: fg,
-      fgValid: valid
-    });
-
-    fg = fg.trim();
-
-    //is it (valid? || zero?) && different? then update
-    if((valid || fg.length == 0) && fg != this.state.fgPrevious) {
-      this.setState({ fgPrevious: fg }, () => {
-        this.props.parentCallback({FG: fg});
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="prediction-container">
-        <div className="recipe-colour">
-          <img
-            src={ glass }
-            style={ {backgroundColor: BrewCalc.srmToHex(this.props.recipe.colour)} }>
-          </img>
-        </div>
-
-        <div className="predictions">
-          <div>
-            <strong>OG: </strong><span className="og-display">{ this.props.recipe.OG }</span>
-            <strong>FG: </strong>
-            <input
-              id="FG"
-              className="fg-model"
-              value={ this.state.fgInput }
-              onChange={ (e) => this.updateFg(e) }
-              title="You may enter up to two gravity values, separated by a '-'. Eg: 1.009-1.012"
-              style={ {color: this.state.fgValid ? 'inherit' : 'red'} }>
-            </input>
-          </div>
-
-          <div>
-            <strong>IBUs: </strong><span className="ibu-display">{ this.props.IBU }</span>
-            <strong>SRM: </strong><span className="srm-display">{ this.props.recipe.colour }</span>
-            <strong>ABV: </strong><span className="abv-display">{ this.props.recipe.abv }</span>%
-          </div>
-        </div>
-      </div>
     );
   }
 }
--- client/app/bundles/RecipeForm/startup/registration.jsx
+++ client/app/bundles/RecipeForm/startup/registration.jsx
@@ -1,5 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 import RecipeForm from '../components/RecipeForm';
+import RecipeMetaPanel from '../components/RecipeMetaPanel';
 import BrewCalc from '../brewCalcs'
 
 ReactOnRails.register({
