6090ea0ef0f7b2cdf9343b00f0e6a9af0e660cd6
Staphsynth <1337dallen@gmail.com>
2017-08-20 20:26:30 +1000

    Add adjuncts to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_malts, :inverse_of => :recipe, :dependent => :destroy
   has_many :recipe_hops, :inverse_of => :recipe, :dependent => :destroy
   has_many :recipe_yeasts, :inverse_of => :recipe, :dependent => :destroy
+  has_many :recipe_adjuncts, :inverse_of => :recipe, :dependent => :destroy
   has_many :ratings, :inverse_of => :recipe, :dependent => :destroy
 
   accepts_nested_attributes_for :recipe_malts, :allow_destroy => true,
