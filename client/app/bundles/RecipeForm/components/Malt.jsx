7bc6d3ce4a27bcbdc700db35806e2232258bc4d6
Staphsynth <1337dallen@gmail.com>
2017-08-21 22:00:09 +1000

    Added Malt << Ingredient component

---
+++ client/app/bundles/RecipeForm/components/Malt.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Ingredient from './Ingredient';
+import Input from './Input';
+
+export default class Malt extends Ingredient {
+  constructor(props) {
+    super(props);
+    this.state = this.props.ingredient;
+  }
+
+  decimalNumberRegex(){
+    return /^[0-9]+(\.[0-9]+)?$/;
+  }
+
+  validateQty(qty) {
+    return this.decimalNumberRegex().test(qty);
+  }
+
+  handleChange(key, value) {
+    var newState = {};
+    newState[key] = value;
+
+    this.setState(newState, () => {
+      this.props.parentCallback(this.state);
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="malt-input">
+        <label htmlFor="malt">Malts and Sugars</label>
+        <select
+          id="malt"
+          value={ this.state.malt }
+          onChange={ e => this.handleChange('malt', e.target.value) }>
+          { this.generateOptions() }
+        </select>
+
+        <label htmlFor="qty">Qty</label>
+        <Input
+          id="qty"
+          valid={ v => this.validateQty(v) }
+          fireChange={ v => this.handleChange('quantity', v) }
+          value={ this.state.quantity } />
+        <span> g</span>
+      </div>
+    );
+  }
+}
