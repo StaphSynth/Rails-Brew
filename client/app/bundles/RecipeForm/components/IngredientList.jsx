521d8037c8bfcf755f8086ce6b5576f5611f407d
Staphsynth <1337dallen@gmail.com>
2017-08-22 10:32:53 +1000

    Add ability to create new ingredient

--- client/app/bundles/RecipeForm/components/IngredientList.jsx
+++ client/app/bundles/RecipeForm/components/IngredientList.jsx
@@ -5,6 +5,8 @@ export default class IngredientList extends React.Component {
   constructor(props) {
     super(props);
     this.generateList = this.generateList.bind(this);
+
+    this.state = { ingredients: this.props.ingredients };
   }
 
   generateList() {
@@ -12,9 +14,11 @@ export default class IngredientList extends React.Component {
     let Ingredient = this.getType();
     let tempValue;
 
-    for(let i = 0; i < this.props.ingredients.length; i++) {
-      tempValue = this.props.ingredients[i];
-      tempValue.ref = i; //set a reference so it can be used to modify state later
+    for(let i = 0; i < this.state.ingredients.length; i++) {
+      tempValue = this.state.ingredients[i];
+      //set a reference to its position in the list
+      //so it can be used to modify state later
+      tempValue.ref = i;
 
       list.push(
         <li key={ i }>
@@ -42,19 +46,43 @@ export default class IngredientList extends React.Component {
     }
   }
 
+  getNewTemplate() {
+    switch(this.props.type) {
+      case 'malt':
+        return new Object({ malt: '', quantity: 0 });
+      case 'hop':
+        return;
+      case 'yeast':
+        return;
+    }
+  }
+
+  createNewIngredient() {
+    let currentList = this.state.ingredients;
+    let newIngredient = this.getNewTemplate(currentList.length);
+    currentList.push(newIngredient);
+
+    this.setState({ ingredients: currentList });
+  }
+
   handleChange(data) {
-    let ingredients = this.props.ingredients;
+    let ingredients = this.state.ingredients;
     let refType = this.props.type + 's';
     //replace the old data value with the new one using the ref given in list generation
     ingredients[data.ref] = data;
-    this.props.parentCallback({ refType: ingredients });
+    this.setState({ ingredients: ingredients }, () => {
+      this.props.parentCallback({ refType: ingredients });
+    });
   }
 
   render() {
     return (
-      <ul className={ this.props.type + '-inputs'}>
-        { this.generateList() }
-      </ul>
+      <div>
+        <ul className={ this.props.type + '-inputs'}>
+          { this.generateList() }
+        </ul>
+        <button onClick={ () => this.createNewIngredient() }>+</button>
+      </div>
     );
   }
 }
